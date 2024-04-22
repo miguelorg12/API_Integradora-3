2f4ba630a5500b2a57d9b6153f495697215937a5
Emith12 <edsonalgarate@gmail.com>
2024-04-21 22:42:40 -0600

    asdas

--- app/Http/Controllers/SQL/Hospitals.php
+++ app/Http/Controllers/SQL/Hospitals.php
@@ -62,26 +62,30 @@ class Hospitals extends Controller
         return response()->json(['msg' => 'Hospital creado'], 201);
     }
 
-    public function update(Request $request, $id)
-    {
-        $hospital = Hospital::where('id', $id)->where('is_active', true)->first();
-        
-        $validator = Validator::make($request->all(), [
-            'nombre' => 'required|string|min:3|max:100|regex:/^[a-zA-Z0-9 ]*$/',
-            'direccion' => 'required|string|min:3|max:100',
-            'telefono' => 'required|string|min:10|max:10|regex:/^[0-9]*$/',
-            'is_active' => 'required|boolean',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['msg' => 'Error en los datos', 'errors' => $validator->errors()], 400);
-        }
-        $hospital->nombre = $request->nombre;
-        $hospital->direccion = $request->direccion;
-        $hospital->telefono = $request->telefono;
-        $hospital->is_active = $request->is_active == '1' ? true : false;
-        $hospital->save();
-        return response()->json(['msg' => 'Hospital actualizado'], 200);
+    
+public function update(Request $request, $id)
+{
+    $hospital = Hospital::where('id', $id)->first();
+    if (!$hospital) {
+        return response()->json(['msg' => 'Hospital no encontrado'], 404);
     }
+    $validator = Validator::make($request->all(), [
+        'nombre' => 'required|string|min:3|max:100|regex:/^[a-zA-Z0-9 ]*$/',
+        'direccion' => 'required|string|min:3|max:100',
+        'telefono' => 'required|string|min:10|max:10|regex:/^[0-9]*$/',
+        'is_active' => 'required|boolean',
+    ]);
+    if ($validator->fails()) {
+        return response()->json(['msg' => 'Error en los datos', 'errors' => $validator->errors()], 400);
+    }
+    $hospital->nombre = $request->nombre;
+    $hospital->direccion = $request->direccion;
+    $hospital->telefono = $request->telefono;
+    $hospital->is_active = $request->is_active == '1' ? true : false;
+    $hospital->save();
+    return response()->json(['msg' => 'Hospital actualizado'], 200);
+}
+
 
     public function destroy($id)
     {
